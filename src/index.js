7584bb8af1f6e110ac28fc3c37c0f496945e3996
unknown <merjembajramovic8@gmail.com>
2020-03-29 23:39:57 +0200

    Pizza js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,79 @@
-const { Survey } = require('enquirer');
+const inquirer = require('inquirer');
 
-const prompt = new Survey({
-    name: 'experience',
-    message: 'Please rate your experience',
-    scale: [
-        { name: '1', message: 'Strongly Disagree' }
-    ],
-    margin: [0, 0, 3, 2, 1],
-    choices: [{
-            name: 'Sethi:',
-            message: 'Sethi is best pandi.'
-        },
-        {
-            name: 'Elypia',
-            message: 'Elypia sucks.'
-        },
-        {
-            name: 'Nikky',
-            message: 'Nikky n Vuki make great art.'
-        },
-        {
-            name: 'Toot',
-            message: 'Margy does best Toot.'
-        },
-        {
-            name: 'Jen',
-            message: 'Jenni sucks at crocheting.'
-        },
-        {
-            name: 'Joe',
-            message: 'Joe is a rock'
-        },
-        {
-            name: 'Jesus',
-            message: 'Jesus best photographer.'
-        },
-        {
-            name: 'Jesu',
-            message: 'Bread and Seven best pastries and they wont be mad Vuki forgot them a little.'
-        }
-    ]
-});
+const choices = [{
+        name: 'Strongly Disagree',
+        value: 0
+    },
+    {
+        name: 'Disagree',
+        value: 1
+    },
+    {
+        name: 'Neutral',
+        value: 2
+    },
+    {
+        name: 'Agree',
+        value: 3
+    },
+    {
+        name: 'Strongly Agree',
+        value: 4
+    }
+];
 
-prompt.run()
-    .then(value => console.log('ANSWERS:', value))
-    .catch(console.error);
+const questions = [{
+        type: 'list',
+        name: 'seth',
+        message: 'Sethi is best pandi.',
+        choices: choices
+    },
+    {
+        type: 'list',
+        name: 'elypia',
+        message: 'Elypia sucks.',
+        choices: choices
+    },
+    {
+        type: 'list',
+        name: 'nikky',
+        message: 'Nikky n Vuki make great art.',
+        choices: choices
+    },
+    {
+        type: 'list',
+        name: 'toot',
+        message: 'Margy does best Toot.',
+        choices: choices
+    },
+    {
+        type: 'list',
+        name: 'jen',
+        message: 'Jenni sucks at crocheting.',
+        choices: choices
+    },
+    {
+        type: 'list',
+        name: 'joe',
+        message: 'Joe is a rock',
+        choices: choices
+    },
+    {
+        type: 'list',
+        name: 'jesus',
+        message: 'Jesus best photographer.',
+        choices: choices
+    },
+    {
+        type: 'list',
+        name: 'jesu',
+        message: 'Bread and Seven best pastries and they wont be mad Vuki forgot them a little.',
+        choices: choices
+    }
+];
+
+console.log('Please rate your experience');
+
+inquirer.prompt(questions)
+    .then(answers => console.log(answers))
+    .catch(error => console.error(error));
