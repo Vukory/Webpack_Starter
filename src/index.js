d729c0ed1944e72284f7820150735498ab54c961
unknown <merjembajramovic8@gmail.com>
2020-03-30 16:01:06 +0200

    isNaN validation for income

--- src/index.js
+++ src/index.js
@@ -1,79 +1,60 @@
 const inquirer = require('inquirer');
 
 const choices = [{
-        name: 'Strongly Disagree',
+        name: 'Weekly',
         value: 0
     },
     {
-        name: 'Disagree',
+        name: 'Monthly',
         value: 1
     },
     {
-        name: 'Neutral',
+        name: 'Annualy',
         value: 2
     },
-    {
-        name: 'Agree',
-        value: 3
-    },
-    {
-        name: 'Strongly Agree',
-        value: 4
-    }
+
 ];
 
 const questions = [{
         type: 'list',
-        name: 'seth',
-        message: 'Sethi is best pandi.',
+        name: 'category',
+        message: 'Please select the cathegory of earnings:',
         choices: choices
     },
     {
-        type: 'list',
-        name: 'elypia',
-        message: 'Elypia sucks.',
-        choices: choices
-    },
-    {
-        type: 'list',
-        name: 'nikky',
-        message: 'Nikky n Vuki make great art.',
-        choices: choices
-    },
-    {
-        type: 'list',
-        name: 'toot',
-        message: 'Margy does best Toot.',
-        choices: choices
-    },
-    {
-        type: 'list',
-        name: 'jen',
-        message: 'Jenni sucks at crocheting.',
-        choices: choices
-    },
-    {
-        type: 'list',
-        name: 'joe',
-        message: 'Joe is a rock',
-        choices: choices
-    },
-    {
-        type: 'list',
-        name: 'jesus',
-        message: 'Jesus best photographer.',
-        choices: choices
-    },
-    {
-        type: 'list',
-        name: 'jesu',
-        message: 'Bread and Seven best pastries and they wont be mad Vuki forgot them a little.',
-        choices: choices
+        type: 'number',
+        message: 'Enter you income',
+        name: 'income',
+        validate: (value) => !Number.isNaN(value)
     }
 ];
 
-console.log('Please rate your experience');
 
+function tax(paidValue) {
+    let taxCalc = 0;
+    let remainingToBeTaxed = paidValue;
+
+    if (remainingToBeTaxed > 150000) {
+        taxCalc += 0.45 * (remainingToBeTaxed - 150000);
+        remainingToBeTaxed = 150000;
+    }
+
+    if (remainingToBeTaxed > 50000) {
+        taxCalc += 0.4 * (remainingToBeTaxed - 50000);
+        remainingToBeTaxed = 50000;
+    }
+
+    if (remainingToBeTaxed > 12500) {
+        taxCalc += 0.2 * (remainingToBeTaxed - 12500);
+    }
+
+    return taxCalc;
+}
+
+// var value_input = parseFloat(prompt("Paid: "));
+// console.log("Tax: " + tax(value_input));
+
+console.log('Please input your earnings:');
 inquirer.prompt(questions)
     .then(answers => console.log(answers))
     .catch(error => console.error(error));
